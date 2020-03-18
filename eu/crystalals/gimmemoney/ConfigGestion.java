8e8557bc0ac738e595cc760bfb87d0903acb76de
Nicolas Went <nicolas.went@epita.fr>
2020-03-18 23:11:27 +0100

    [Config] Added gestion to get money

--- eu/crystalals/gimmemoney/ConfigGestion.java
+++ eu/crystalals/gimmemoney/ConfigGestion.java
@@ -2,6 +2,7 @@ package eu.crystalals.gimmemoney;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -12,6 +13,7 @@ public class ConfigGestion
 	private GimmeMoney plugin;
 	private File file;
 	private FileConfiguration customConfig;
+	private Random random = new Random();
 	
 	
 	public ConfigGestion(GimmeMoney plugin)
@@ -36,6 +38,32 @@ public class ConfigGestion
 		customConfig.addDefault("cow.MaxMoney", 40.00);
 		customConfig.options().copyDefaults(true);
 		save();
+	}
+	
+	public boolean isEntityGivingMoney(String name)
+	{
+		return customConfig.contains(name);
+	}
+	
+	/*
+	 * This function get the money given by an entity,
+	 * it set the return value to random from MinMoney and MaxMoney.
+	 */
+	public double getMoney(String name)
+	{
+		double min = customConfig.getDouble(name + ".MinMoney");
+		double max = customConfig.getDouble(name + ".MaxMoney");
+		double ret = min + (max - min) * random.nextDouble();
+		return round(ret, plugin.getConfig().getInt("AfterDotNumber"));
+	}
+	
+	/*
+	 * Allow us to round the given number at n decimals
+	 */
+	private double round(double to_round, int n)
+	{
+		double power_ten = Math.pow(to_round, n);
+		return ((double)Math.round(to_round * power_ten)) / power_ten;
 	}
 	
 	private void createNewFile()
